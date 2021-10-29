ff3bface506f14535f9241884f53e837d251d64a
= <=>
2021-10-29 16:11:51 +0500

    Adminlte 3

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,8 +13,16 @@ return [
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@backend/theme'
+                ],
+            ],
+        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
+            'baseUrl' => '/admin    '
         ],
         'user' => [
             'identityClass' => 'common\models\User',
@@ -37,14 +45,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
